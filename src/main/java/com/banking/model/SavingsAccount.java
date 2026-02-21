7c0eb330d5655d76fc43961241fd847bedd38415
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-21 22:37:50 +0530

    ""

--- src/main/java/com/banking/model/SavingsAccount.java
+++ src/main/java/com/banking/model/SavingsAccount.java
@@ -2,4 +2,15 @@ package com.banking.model;
 
 public class SavingsAccount extends Account {
     private final double minBalanceForInterest;
+    private SavingsAccount(Builder builder){
+        super(builder);
+        this.minBalanceForInterest=builder.minBalanceForInterest;
+    }
+    @Override
+    public double calculateInterest(){
+        if(getBalance()>=minBlanaceForInterest){
+            double interest = getBalance()*getInterestRate();
+
+        }
+    }
 }
