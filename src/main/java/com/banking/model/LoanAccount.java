1d57e1128f2f9a6f35d88549f780b4f3b8112c36
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-22 17:52:19 +0530

    ""

--- src/main/java/com/banking/model/LoanAccount.java
+++ src/main/java/com/banking/model/LoanAccount.java
@@ -1,5 +1,10 @@
 package com.banking.model;
 
 public class LoanAccount extends Account {
-
+private final double originalLoanAmount;
+private LoanAccount(Builder builder){
+    super(builder);
+    this.originalLoanAmount=builder.originalLoanAmount;
+}
+@Override
 }
