ed95664539833abc820eaa247a8f5ca981e5b4e7
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-22 17:54:52 +0530

    ""

--- src/main/java/com/banking/model/LoanAccount.java
+++ src/main/java/com/banking/model/LoanAccount.java
@@ -7,4 +7,15 @@ private LoanAccount(Builder builder){
     this.originalLoanAmount=builder.originalLoanAmount;
 }
 @Override
+    public void deposit(double amount){
+    if(amount<=0){
+        throw new IllegalArgumentException("Payment amount must be positive");
+    }
+    if(amount>getBalance()){
+        throw new IllegalArgumentException("Payment exceeds remaining loan balance");
+    }
+    setBalance(getBalance()-amount);
+    recordTransaction(TransactionType.DEPOSIT,amount,"Loan Payment");
+}
+@Override
 }
