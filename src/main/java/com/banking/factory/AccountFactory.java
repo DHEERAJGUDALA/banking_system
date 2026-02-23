067c74d18436ab7debe724e11668df7773d93645
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-23 21:18:13 +0530

    "Created AccountFactory class"

--- src/main/java/com/banking/factory/AccountFactory.java
+++ src/main/java/com/banking/factory/AccountFactory.java
@@ -10,4 +10,19 @@ public class AccountFactory {
     private static final double DEFUALT_LOAN_INTEREST=0.08;
     private static final double DEFAULT_OVERDRAFT_LIMIT=50000;
 
+    public static Account createAccount(AccountType type, String accountId, String holderName,double initialAmount){
+        return switch(type){
+            case SAVINGS -> new SavingsAccount.Builder(accountId,holderName)
+                    .balance(initialAmount)
+                    .interestRate(DEFAULT_SAVINGS_INTEREST)
+                    .build();
+            case CURRENT -> new CurrentAccount.Builder(accountId,holderName)
+                    .balance(initialAmount)
+                    .overdraftLimit(DEFAULT_OVERDRAFT_LIMIT)
+                    .build();
+            case LOAN -> new LoanAccount.Builder(accountId,holderName,initialAmount)
+                    .interestRate(DEFAULT_LOAN_INTEREST)
+                    .build();
+        };
+    }
 }
