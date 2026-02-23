87244a3825f92fcb64f500824dc2a50cfea3f06c
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-23 21:12:23 +0530

    ""

--- src/main/java/com/banking/factory/AccountFactory.java
+++ src/main/java/com/banking/factory/AccountFactory.java
@@ -9,4 +9,5 @@ public class AccountFactory {
     private static final double DEFAULT_SAVINGS_INTEREST=0.04;
     private static final double DEFUALT_LOAN_INTEREST=0.08;
     private static final double DEFAULT_OVERDRAFT_LIMIT=50000;
+
 }
