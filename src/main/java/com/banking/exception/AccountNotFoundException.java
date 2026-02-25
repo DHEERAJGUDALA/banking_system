b4925e0f0bc51ffa807b9fdafc0e5686da534f36
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-25 19:55:20 +0530

    "Created AccountNotFoundException class"

---
+++ src/main/java/com/banking/exception/AccountNotFoundException.java
@@ -0,0 +1,12 @@
+package com.banking.exception;
+
+public class AccountNotFoundException extends RuntimeException {
+    private final String accountId;
+
+    public AccountNotFoundException(String accountId) {
+        super("Account not found: " + accountId);
+        this.accountId = accountId;
+    }
+
+    public String getAccountId() { return accountId; }
+}
